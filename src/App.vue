84a0fcf02bc1a494921ecaa1f8f6376397233ece
Irene Veselova <ik1vikuz@gmail.com>
2022-02-27 23:51:52 +0300

    container & header added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script>
 export default {
   render(h) {
-    return h("h1", { class: "p-5 w-[32rem] text-sky-400" }, "App")
+    return h("div", { class: { 'block w-full h-screen bg-faint' : true } }, [
+      h("div", { class: { 'container block w-full max-w-2xl m-auto px-2' : true } }, [
+        h("section", { class: { 'bg-white' : true } }, [
+          h("header", { class: { 'w-full flex justify-between p-5 border-b border-grey' : true } }, [
+            h("div", { class: { 'w-1/3 h-20 bg-black' : true } },),
+            h("div", { class: { 'fle flex-col w-1/3 h-20 text-center' : true } },[
+              h("h2", { class: { ' text-sm font-medium' : true },},'🦄 Team Unicorns'),
+              h("p", { class: { ' text-xs' : true },},'last seen 45 minutes ago'),
+            ]
+
+            ),
+            h("div", { class: { 'text-right w-1/3 h-20' : true } },[
+              h("p", { class: { ' text-3xl text-grey' : true },},'...'),
+            ]),
+          ])
+        ])
+      ])
+    ])
   }
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        faint: {
+          DEFAULT: '#F2F2F7'
+        }
+      }
+    },
   },
   plugins: [],
 }
